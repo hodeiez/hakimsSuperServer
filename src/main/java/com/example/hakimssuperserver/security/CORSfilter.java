c74f81a079b280a5e223f65582c0d158f2a2ae2d
hodeiez <hodeinaiz@hotmail.com>
2021-09-15 11:25:06 +0200

    added corsfilter

---
+++ src/main/java/com/example/hakimssuperserver/security/CORSfilter.java
@@ -0,0 +1,59 @@
+package com.example.hakimssuperserver.security;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Hodei Eceiza
+ * Date: 9/15/2021
+ * Time: 11:23
+ * Project: hakimsSuperServer
+ * Copyright: MIT
+ */
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSfilter implements Filter {
+
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
+            try {
+                chain.doFilter(req, res);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            System.out.println("Pre-flight");
+            response.setHeader("Access-Control-Allowed-Methods", "POST, GET, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type,x-auth-token, " +
+                    "access-control-request-headers, access-control-request-method, accept, origin, authorization, x-requested-with");
+
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+
+}
